4c8e00b81a6a289cd2e1f0d9de2d68a5f58f79e0
q2q2q8 <art-kash@mail.ru>
2022-11-21 10:15:39 +0300

    проверьте, пожалуйста

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
+import java.time.LocalDate;
+
 public class Main {
+    public static final int nowYear = LocalDate.now().getYear();
     public static void main(String[] args) {
         int meteorite = 0;
-        int year200 = 2022 - 200;
-        int year100 = 2022 + 100;
+        int startYear = nowYear - 200;
+        int finalYear = nowYear + 100;
         for (int year = 0; year <= 2022; year++) {
             meteorite = 79 + meteorite;
-            year = meteorite;
-             if (meteorite >= year200 && meteorite <= year100) {
+             if (meteorite >= startYear && meteorite <= finalYear) {
                  System.out.println(meteorite);
              }
 
